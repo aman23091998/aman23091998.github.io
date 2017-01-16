3a2651e9e0329977686f994725d7b6a67886df81
Aman <aman23091998@gmail.com>
2017-01-15 19:59:33 -0500

    Move sections around

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 var $root = $('#main');
 
+var viewport = {
+  width  : $(window).width(),
+  height : $(window).height()
+};
+
+var navBarHeight = (viewport.width <= 600) ? 40: 80;
+
 var changeNavState = function(item_id){
   $('#nav_projects').removeClass("navigation-bar-active");
   $('#nav_home').removeClass("navigation-bar-active");
@@ -7,15 +14,19 @@ var changeNavState = function(item_id){
   $(item_id).addClass("navigation-bar-active");
 };
 
-function scrollPage(item, item_id){
-  var scrollTopValue = $root.scrollTop()  + $(item).offset().top - 80;
+function scrollTo(item){
+  var scrollTopValue = $root.scrollTop()  + $(item).offset().top - navBarHeight;
   $root.animate({
     scrollTop: scrollTopValue
   }, 1000);
+}
+
+function scrollPage(item, item_id){
+  scrollTo(item);
   changeNavState(item_id);
   return true;
 }
-$("#down-arrow, #nav_projects").on('click', function(){
+$("#nav_projects").on('click', function(){
   scrollPage('#projects', '#nav_projects');
 });
 $('#nav_home').on('click', function(){
@@ -24,14 +35,16 @@ $('#nav_home').on('click', function(){
 $('#nav_about').on('click', function(){
   scrollPage('#about', '#nav_about');
 });
+$('#down-arrow').on('click', function(){
+  scrollTo('#about');
+});
 
 var homeOffset = $('#home').offset().top;
 var projectsOffset = $('#projects').offset().top;
 var aboutOffset = $('#about').offset().top;
 
 $root.on('scroll', function(){
-  console.log($root.scrollTop());
-  if($root.scrollTop() + 20 >= aboutOffset - 80)changeNavState('#nav_about');
-  else if ($root.scrollTop() + 20 >= projectsOffset - 80)changeNavState('#nav_projects');
+  if ($root.scrollTop() + 20 >= projectsOffset - navBarHeight)changeNavState('#nav_projects');
+  else if($root.scrollTop() + 20 >= aboutOffset - navBarHeight)changeNavState('#nav_about');
   else changeNavState('#nav_home');
 });
